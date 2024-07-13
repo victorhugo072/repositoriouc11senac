8532f092e46798cf639720036e9f6860d8f0ff04
victorhugo072 <victorhugofigueiredorios1563@gmail.com>
2024-07-13 15:09:56 -0300

    add funcionalidade cadastrar

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -7,36 +7,57 @@
  *
  * @author Adm
  */
-
-import java.sql.PreparedStatement;
-import java.sql.Connection;
+import java.sql.*;
 import javax.swing.JOptionPane;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+        conn = new conectaDAO().connectDB();
+        try {
+            // Preparando a query SQL para inserção de um novo filme
+            prep = conn.prepareStatement("INSERT INTO produtos "
+                    + "(nome, valor, status) "
+                    + "VALUES "
+                    + "(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+
+            // Setando os parâmetros da query com os dados do filme
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            // Executando a query
+            prep.executeUpdate();
+
+            conn.close();
+            JOptionPane.showMessageDialog(null, "Produto salvo com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
